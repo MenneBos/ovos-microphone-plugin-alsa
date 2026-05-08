b8dcc0a46048a6cafedba7d75cf0c8a38ce1acf7
MenneBos <mennebos@proton.me>
2026-05-09 01:31:41 +0200

    Change audio format for denoising process
    Updated audio processing to use original audio instead of int16 format for denoising.

--- ovos_microphone_plugin_alsa/__init__.py
+++ ovos_microphone_plugin_alsa/__init__.py
@@ -76,14 +76,14 @@ class AlsaMicrophone(Microphone):
         #audio = np.diff(audio, prepend=self._prev_sample) * 0.97
         #self._prev_sample = float(audio[-1])
     
-        audio_int16 = audio.astype(np.int16)
+        #audio = audio.astype(np.int16)
     
         # ============================
         # RNNoise streaming chunk API
         # ============================
         denoised_output = []
     
-        for speech_prob, denoised_audio in self.denoiser.denoise_chunk(audio_int16):
+        for speech_prob, denoised_audio in self.denoiser.denoise_chunk(audio):
             LOG.debug(f"Speech probability: {speech_prob}")
     
             # je kunt hier later VAD gating doen
