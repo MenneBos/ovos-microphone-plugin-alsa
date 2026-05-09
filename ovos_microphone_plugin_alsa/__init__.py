0e3f113e506d7b19fb1bb9181fe98c370e9d7030
MenneBos <mennebos@proton.me>
2026-05-09 20:11:17 +0200

    Update audio preprocessing and buffer size
    Commented out audio processing steps and increased max buffer size.

--- ovos_microphone_plugin_alsa/__init__.py
+++ ovos_microphone_plugin_alsa/__init__.py
@@ -67,10 +67,10 @@ class AlsaMicrophone(Microphone):
     def _preprocess_audio(self, chunk_bytes):
         audio_data = np.frombuffer(chunk_bytes, dtype=np.int16)
     
-        audio = audio_data.astype(np.float32)
+        #audio = audio_data.astype(np.float32)
     
         # DC removal
-        audio -= np.mean(audio)
+        #audio -= np.mean(audio)
 
         # snelle high-pass (vectorized)
         #audio = np.diff(audio, prepend=audio[0]) * 0.97
@@ -81,7 +81,7 @@ class AlsaMicrophone(Microphone):
         #audio = np.diff(audio, prepend=self._prev_sample) * 0.97
         #self._prev_sample = float(audio[-1])
     
-        audio = audio.astype(np.int16)
+        #audio = audio.astype(np.int16)
         audio = np.expand_dims(audio, axis=0)  # (1, N)
     
         # ============================
@@ -163,7 +163,7 @@ class AlsaMicrophone(Microphone):
                             self.sample_channels
                         )
                         
-                        max_buffer_size = bytes_per_second * 10  # 10 seconden audio
+                        max_buffer_size = bytes_per_second * 20  # 10 seconden audio
                         
                         debug_saved = False
                         
