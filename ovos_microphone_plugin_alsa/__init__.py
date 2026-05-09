4337e941380afc74cfefe61c7a86bf1560ecb023
MenneBos <mennebos@proton.me>
2026-05-09 02:01:26 +0200

    Clean up denoising function by removing comments
    Removed commented-out code for normalizing audio.

--- ovos_microphone_plugin_alsa/__init__.py
+++ ovos_microphone_plugin_alsa/__init__.py
@@ -91,6 +91,11 @@ class AlsaMicrophone(Microphone):
     
         for denoised_audio in self.denoiser.denoise_chunk(audio):
             denoised_output.append(denoised_audio)
+            # normalize to 1D
+            #if denoised_audio.ndim == 2:
+            #    denoised_audio = denoised_audio[0]
+
+            denoised_output.append(denoised_audio)
     
         if not denoised_output:
             return b""
