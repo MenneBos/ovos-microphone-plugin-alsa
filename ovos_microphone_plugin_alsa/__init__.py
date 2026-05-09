ce505805592d3f8a68f2a131771404289f2945ab
MenneBos <mennebos@proton.me>
2026-05-09 02:03:34 +0200

    Fix normalization condition for denoised audio

--- ovos_microphone_plugin_alsa/__init__.py
+++ ovos_microphone_plugin_alsa/__init__.py
@@ -92,8 +92,8 @@ class AlsaMicrophone(Microphone):
         for denoised_audio in self.denoiser.denoise_chunk(audio):
             denoised_output.append(denoised_audio)
             # normalize to 1D
-            #if denoised_audio.ndim == 2:
-            #    denoised_audio = denoised_audio[0]
+            if denoised_audio.ndim == 2:
+                denoised_audio = denoised_audio[0]
 
             denoised_output.append(denoised_audio)
     
